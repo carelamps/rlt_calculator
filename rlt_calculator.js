a5bc2dfe0f36c8b2812262d2181a4f01cc44e69f
carelamps <137308497+carelamps@users.noreply.github.com>
2023-06-21 15:59:06 +0100

    Update rlt_calculator.js

--- rlt_calculator.js
+++ rlt_calculator.js
@@ -1,22 +1,33 @@
+function calculateTreatmentTime(dosage, powerDensity) {
+  if (isNaN(dosage) || isNaN(powerDensity) || powerDensity === 0) {
+    return null;
+  }
+
+  const treatmentTime = dosage / (powerDensity / 1000);
+  const minutes = Math.floor(treatmentTime / 60);
+  const seconds = Math.ceil(treatmentTime % 60);
+
+  return {
+    minutes,
+    seconds
+  };
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const button = document.querySelector('.rlt-calc-button');
-  button.addEventListener('click', calculateTreatmentTime);
+  button.addEventListener('click', function() {
+    const dosageInput = document.querySelector('.rlt-dosage-input');
+    const irradianceInput = document.querySelector('.rlt-irradiance-input');
+    const resultElement = document.querySelector('.rlt-result');
+
+    const dosage = parseFloat(dosageInput.value);
+    const powerDensity = parseFloat(irradianceInput.value);
+
+    const treatmentTime = calculateTreatmentTime(dosage, powerDensity);
+    if (treatmentTime) {
+      resultElement.textContent = `Treatment Time: ${treatmentTime.minutes} minutes ${treatmentTime.seconds} seconds`;
+    } else {
+      resultElement.textContent = "Please enter valid numbers for dosage and power density.";
+    }
+  });
 });
-
-function calculateTreatmentTime() {
-  const dosageInput = document.querySelector('.rlt-dosage-input');
-  const irradianceInput = document.querySelector('.rlt-irradiance-input');
-  const resultElement = document.querySelector('.rlt-result');
-
-  const dosage = parseFloat(dosageInput.value);
-  const powerDensity = parseFloat(irradianceInput.value);
-
-  if (isNaN(dosage) || isNaN(powerDensity) || powerDensity === 0) {
-    resultElement.textContent = "Please enter valid numbers for dosage and power density.";
-  } else {
-    const treatmentTime = dosage / powerDensity * 60; // time in seconds
-    const minutes = Math.floor(treatmentTime / 60);
-    const seconds = Math.floor(treatmentTime % 60);
-    resultElement.textContent = `Treatment Time: ${minutes} minutes ${seconds} seconds`;
-  }
-}
