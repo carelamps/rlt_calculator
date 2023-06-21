4f0d5bd47942d43f80142b497a7b4c4d69c58e37
carelamps <137308497+carelamps@users.noreply.github.com>
2023-06-21 15:57:11 +0100

    Update rlt_calculator.js

--- rlt_calculator.js
+++ rlt_calculator.js
@@ -1,14 +1,22 @@
-function calculateTreatmentTime(dosage, powerDensity) {
+document.addEventListener('DOMContentLoaded', function() {
+  const button = document.querySelector('.rlt-calc-button');
+  button.addEventListener('click', calculateTreatmentTime);
+});
+
+function calculateTreatmentTime() {
+  const dosageInput = document.querySelector('.rlt-dosage-input');
+  const irradianceInput = document.querySelector('.rlt-irradiance-input');
+  const resultElement = document.querySelector('.rlt-result');
+
+  const dosage = parseFloat(dosageInput.value);
+  const powerDensity = parseFloat(irradianceInput.value);
+
   if (isNaN(dosage) || isNaN(powerDensity) || powerDensity === 0) {
-    return null;
+    resultElement.textContent = "Please enter valid numbers for dosage and power density.";
+  } else {
+    const treatmentTime = dosage / powerDensity * 60; // time in seconds
+    const minutes = Math.floor(treatmentTime / 60);
+    const seconds = Math.floor(treatmentTime % 60);
+    resultElement.textContent = `Treatment Time: ${minutes} minutes ${seconds} seconds`;
   }
-
-  const treatmentTime = dosage / powerDensity * 60; // time in seconds
-  const minutes = Math.floor(treatmentTime / 60);
-  const seconds = Math.floor(treatmentTime % 60);
-
-  return {
-    minutes,
-    seconds
-  };
 }
